bc91a1714a0f4054cc18f02dc679163159549288
Vardaan <vardaan119@gmail.com>
2019-01-11 19:05:50 +0530

    small change to test follow up deployments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ yo: 'yo' });
+  res.send({ update: '2' });
 })
 
 const PORT = process.env.PORT || 5000;
