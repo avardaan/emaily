3503f99524af145586da4ac299820d85c6e7358c
Vardaan <vardaan119@gmail.com>
2019-01-13 17:32:29 +0530

    finalize google oauth flow

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const secrets = require('./secrets/secrets');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+  {
+    clientID: secrets.googleOauth.clientID,
+    clientSecret: secrets.googleOauth.clientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile) => {
+    console.log(accessToken, refreshToken, profile);
+  }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log('listening...'));
