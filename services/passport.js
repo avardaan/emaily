1b9823c4298d671996f832a6e7ecfd3ae62825ac
Vardaan <vardaan119@gmail.com>
2019-02-11 22:08:27 -0500

    [ADD] mongoose, connect with mongoDB atlas, save logged in user to database

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const secrets = require('../config/secrets');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -12,7 +14,10 @@ passport.use(
   // callback is run after passport receives code from google (once the user allows),
   // and uses it to fetch an access token
   (accessToken, refreshToken, profile) => {
-    console.log(accessToken);
+    User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      !existingUser ? new User({ googleId: profile.id }).save() : console.log('The user you are trying to create already exists');
+    })
   }
   )
 );
