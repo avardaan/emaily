c4d7b553b587599b12e74670e99f288a04c63aa3
Vardaan <vardaan119@gmail.com>
2019-02-14 22:04:09 -0500

    [HOTFIX] don't ignore keys/index.js

---
+++ config/keys/index.js
@@ -0,0 +1,8 @@
+/* return keys based on environment */
+if (process.env.NODE_ENV === 'production') {
+  // return prod keys
+  module.exports = require('./prod');
+} else {
+  // return dev keys
+  module.exports = require('./dev');
+}
