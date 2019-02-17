8ed6e2cfaa474508aa2603be1ecb9a50dbc87903
Vardaan <vardaan119@gmail.com>
2019-02-17 15:08:33 -0500

    [ADD] redux init

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
 
+const store = createStore(() => [], {}, applyMiddleware());
+const AppWithProvider = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+
 ReactDOM.render(
-  <App />,
+  <AppWithProvider />,
   document.getElementById('root')
 );
