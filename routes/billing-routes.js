8913f194cbdd968c643e2b28c1fca8cd09f90f6c
Vardaan Aashish <vardaan119@gmail.com>
2019-02-24 17:51:47 -0500

    [ADD] create and send charge object

--- routes/billing-routes.js
+++ routes/billing-routes.js
@@ -3,7 +3,13 @@ const stripe = require('stripe')(keys.stripe.secretKey);
 
 module.exports = (app) => {
   
-  app.post('/api/stripe', (req, res) => {
-
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: 'Purchase 5 emaily credits',
+      source: req.body.id,
+    })
+    console.log(charge)
   });
 }
