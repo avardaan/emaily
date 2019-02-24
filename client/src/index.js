6037bee727e77e0947129c3b224ba5db81548650
Vardaan Aashish <vardaan119@gmail.com>
2019-02-23 22:30:52 -0500

    [ADD] setup redux-devtools

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 
 import App from './components/app';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = () => {
+  // enable redux devtools if not in prod env
+  const middleware = [reduxThunk];
+  const applyMiddlewareToStore = process.env.NODE_ENV === 'production' ?
+    applyMiddleware(...middleware) :
+    composeWithDevTools(applyMiddleware(...middleware));
+  return createStore(reducers, {}, applyMiddlewareToStore);
+}
+
 const AppWithProvider = () => {
   return (
-    <Provider store={store}>
+    <Provider store={store()}>
       <App />
     </Provider>
   )
