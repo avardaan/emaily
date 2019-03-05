7127109cf827e8e70ab7584425b73dd3a4986e5f
Vardaan Aashish <vardaan119@gmail.com>
2019-03-04 21:11:12 -0500

    [ADD] import dashboard in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from './header';
 import Landing from './landing';
+import Dashboard from './dashboard';
 import { authActions } from '../actions/auth-actions/index'
 import 'materialize-css/dist/css/materialize.min.css';
 
-
-const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends React.Component {
