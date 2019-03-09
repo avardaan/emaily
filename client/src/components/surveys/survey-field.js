48637d031f43981d95d1717697a22c0a15981638
Vardaan Aashish <vardaan119@gmail.com>
2019-03-09 14:59:27 -0500

    [ADD] error text styling

--- client/src/components/surveys/survey-field.js
+++ client/src/components/surveys/survey-field.js
@@ -6,8 +6,10 @@ export const SurveyField = ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{ marginBottom: '5px' }} />
+      <div className="red-text" style={{ marginBottom: '20px' }}>
       {touched && error}
+      </div>
     </div>
   );
 }
