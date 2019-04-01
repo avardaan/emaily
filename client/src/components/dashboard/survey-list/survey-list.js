35b7cc8bab6732f95cd665824d168c069cf1bd17
Vardaan Aashish <vardaan119@gmail.com>
2019-03-31 23:07:19 -0400

    [ADD] render surveys in dashboard

--- client/src/components/dashboard/survey-list/survey-list.js
+++ client/src/components/dashboard/survey-list/survey-list.js
@@ -5,7 +5,25 @@ export class SurveyList extends React.PureComponent {
     this.props.fetchSurveys();
   };
 
+  renderSurveys() {
+    return this.props.surveys.map((survey) => (
+      <div className='card darken-1' key={survey._id}>
+        <div className='card-content'>
+          <span className='card-title'>{survey.title}</span>
+          <p>{survey.body}</p>
+          <p className='right'>
+            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
+        </div>
+        <div className='card-action'>
+          <p>Yes: {survey.responses.yes}</p>
+          <p>No: {survey.responses.no}</p>
+        </div>
+      </div>
+    ));
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
