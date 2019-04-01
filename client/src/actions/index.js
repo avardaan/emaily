c2d66989b311da6c27f8ac63ed9c70cb4db30242
Vardaan Aashish <vardaan119@gmail.com>
2019-03-31 22:26:46 -0400

    [ADD] action to fetch surveys for current user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,4 @@
 export * from './auth-actions';
 export * from './payments-actions';
 export * from './form-actions';
+export * from './survey-actions';
---
+++ client/src/actions/survey-actions/index.js
@@ -0,0 +1,3 @@
+import * as surveyActionsTypes from './types';
+import * as surveyActions from './creators';
+export { surveyActions, surveyActionsTypes };
